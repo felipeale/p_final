5911e5c18222d693cdcef75d5653f44947d0bfd6
Felipe <luisfelipe.arias@yahoo.com>
2016-08-03 01:51:09 -0600

    new file:   datos_graf/graf-0.dat 	new file:   datos_graf/graf-1.dat 	new file:   datos_graf/graf-2.dat 	new file:   datos_graf/graf-3.dat 	new file:   datos_graf/graf-4.dat 	new file:   error2.log 	modified:   problema1.sh

--- Proyecto-de-Programacion/problema1/hojasDatos/problema1.sh
+++ Proyecto-de-Programacion/problema1/hojasDatos/problema1.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-DATA=/home/farias/Downloads/Proyecto-de-Programacion/problema1/hojasDatos
-mkdir $DATA/archivos_csv
-
+DATA=/home/farias/git/p_final/Proyecto-de-Programacion/problema1/hojasDatos
 OUT_DATA=$DATA/archivos_csv
+GRAF_DATA=$DATA/datos_graf
+
+mkdir $DATA/archivos_csv
+mkdir $GRAF_DATA
 
 m=0
 
 for i in `find $DATA -name '*.xls' `	
 do 
 	echo "Procesando Archivo $i"
-
-
 	xls2csv $i > $OUT_DATA/data-$m.csv
 	let m=m+1
-done 2> error1.log 
+done 2> error1.log
+
+m=0
+
+for e in `find $OUT_DATA -name "*.csv"`
+do
+	echo "Dando formato de datos para graficar el archivo $e"
+	cat $e | awk -F "\",\"" '{print $1 " " $2 " " $3 " " $4 " " $5}' | sed '1,$ s/"//g' | sed '1 s/date/#date/g' > $GRAF_DATA/graf-$m.dat
+		let m=m+1
+done 2> error2.log
+ 
